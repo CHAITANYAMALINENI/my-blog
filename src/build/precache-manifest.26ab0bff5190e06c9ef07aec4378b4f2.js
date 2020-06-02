ec24d8334d9fe41db98d3a64c6b3551bc2d69e6b
CHAITANYAMALINENI <chaitanyakumar488@gmail.com>
2020-06-02 18:51:01 -0500

    First Commit

---
+++ src/build/precache-manifest.26ab0bff5190e06c9ef07aec4378b4f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c8e39a8b88535bc0190cb0b0158bfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7d23e28dc84f62ed179",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "6b24c0248e4cc554c66b",
+    "url": "/static/js/2.99650adb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.99650adb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7d23e28dc84f62ed179",
+    "url": "/static/js/main.aeb6c30c.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
